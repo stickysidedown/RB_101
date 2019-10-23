2d835b0c31bd48a228e36cdab350da91c768078e
Matt <matthew.elkins@gmail.com>
2019-10-23 11:38:09 -0700

    rubocop fixed

--- RB_101_Lesson_2/RB_101_Lesson_2_Calculator.rb
+++ RB_101_Lesson_2/RB_101_Lesson_2_Calculator.rb
@@ -4,80 +4,81 @@ MESSAGES = YAML.load_file('calculator_messages.yml')
 LANGUAGE = 'en'
 
 def language(message, lang = 'en')
-	MESSAGES[lang][message]
+  MESSAGES[lang][message]
 end
 
 def prompt(message)
-	Kernel.puts("=> #{message}")
+  Kernel.puts("=> #{message}")
 end
 
 def valid_number?(num)
-	num.to_f.to_s == num || num.to_i.to_s == num
+  num.to_f.to_s == num || num.to_i.to_s == num
 end
 
 def valid_prompt?(operation)
-	["1","2","3","4"].include?(operation)
+  ["1", "2", "3", "4"].include?(operation)
 end
 
 def operation_to_message(operator)
-	case operator
-		when "1"
-			return "Adding"
-		when "2"
-			return "Subracting"
-		when "3"
-			return "Multiplying"
-		when "4"
-			return "Dividing"
-	end
+  case operator
+  when "1"
+    "Adding"
+  when "2"
+    "Subracting"
+  when "3"
+    "Multiplying"
+  when "4"
+    "Dividing"
+  end
 end
 
 prompt(language('welcome', LANGUAGE))
 
 name = ""
-loop do
-name = Kernel.gets().chomp()
 
-	if name.empty?()
-		prompt(language('valid name', LANGUAGE))
-	else
-		break
-	end
+loop do
+  name = Kernel.gets().chomp()
+
+  if name.empty?()
+    prompt(language('valid name', LANGUAGE))
+  else
+    break
+  end
 end
 
 prompt("Hi #{name}!")
 
-loop do #Main Outer Loop
-
-#Collecting INPUTS
-number_1 = ""
-
+# Main Outer Loop
 loop do
-	prompt(language('first number?', LANGUAGE))
-	number_1 = Kernel.gets().chomp()
+  # Collecting INPUTS
+  number1 = ""
 
-	if valid_number?(number_1)
-		break
-	else
-		prompt(language('invalid number', LANGUAGE))
-	end
-end
+  loop do
+    prompt(language('first number?', LANGUAGE))
+    number1 = Kernel.gets().chomp()
 
-number_2 = ""
+    if valid_number?(number1)
+      break
+    else
+      prompt(language('invalid number', LANGUAGE))
+    end
+  end
 
-loop do
-prompt(language('second number?', LANGUAGE))
-number_2 = Kernel.gets().chomp()
+  number2 = ""
 
-	if valid_number?(number_2)
-		break
-	else
-		prompt(language('invalid number', LANGUAGE))
-	end
-end
+  loop do
+    prompt(language('second number?', LANGUAGE))
+    number2 = Kernel.gets().chomp()
 
-operator = ""
-operator_prompt = <<-MSG
+    if valid_number?(number2)
+      break
+    else
+      prompt(language('invalid number', LANGUAGE))
+    end
+  end
+
+  operator = ""
+  operator_prompt = <<-MSG
 What operation would you like to perform?
 1) add
 2) subtract
@@ -85,35 +86,36 @@ What operation would you like to perform?
 4) divide
 MSG
 
-loop do
-prompt(operator_prompt)
+  loop do
+    prompt(operator_prompt)
 
-operator = Kernel.gets().chomp()
-	if valid_prompt?(operator)
-		break
-	else
-		prompt(language('invalid operator', LANGUAGE))
-	end
-end
+    operator = Kernel.gets().chomp()
 
-puts "#{operation_to_message(operator)} the numbers..."
+    if valid_prompt?(operator)
+      break
+    else
+      prompt(language('invalid operator', LANGUAGE))
+    end
+  end
 
-#Input operation
+  puts "#{operation_to_message(operator)} the numbers..."
 
-result = case operator
-		 		 when "1"
-	 			 	  number_1.to_i() + number_2.to_i()
-		     when "2"
-				 		number_1.to_i() - number_2.to_i()
-				 when "3"
-				 		number_1.to_i() * number_2.to_i()
-				 when "4"
-				 		number_1.to_f() / number_2.to_f()
-				 end
-# Output
-prompt("The answer is #{result}.")
-prompt(language('another calculation?', LANGUAGE))
-answer = Kernel.gets().chomp
-break unless answer.downcase().start_with?("y")
+  # Input operation
+
+  result =  case operator
+            when "1"
+              number1.to_i() + number2.to_i()
+            when "2"
+              number1.to_i() - number2.to_i()
+            when "3"
+              number1.to_i() * number2.to_i()
+            when "4"
+              number1.to_f() / number2.to_f()
+            end
+  # Output
+  prompt("The answer is #{result}.")
+  prompt(language('another calculation?', LANGUAGE))
+  answer = Kernel.gets().chomp
+  break unless answer.downcase().start_with?("y")
 end
 prompt(language('thank you', LANGUAGE))
